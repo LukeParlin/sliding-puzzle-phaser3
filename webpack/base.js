f8b77aceae2bc7242c82b13d8209422ebe8ffcdf
Luke.Parlin <35572904+LukeParlin@users.noreply.github.com>
2019-09-13 09:47:37 +0100

    Updated package.json and webpack config for devpack access over the local network

--- webpack/base.js
+++ webpack/base.js
@@ -27,6 +27,11 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    host: '0.0.0.0',
+    port: '8080',
+    public: '0.0.0.0:8080'
+  },
   plugins: [
     new CleanWebpackPlugin(["dist"], {
       root: path.resolve(__dirname, "../")
